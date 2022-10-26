074172a054e4033104db157ddd54867fdb2e1ac2
IsabellaSchoen <schoen.isabella@gmx.net>
2022-10-26 18:27:29 +0200

    :nail_care: changed navbar & style

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div>
+    <div v-for="navbar in navbars" :key="navbar.id">
+      <router-link :to="navbar.router">{{ navbar.names }}</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  data: function () {
+    return {
+      navbars: [
+      { names: "Home", router:"/" },
+      { names: "Einstellungen", router:"/einstellungen"}
+      ]
+    }
+  }
+};
+</script>
 
 <style>
 #app {
@@ -14,16 +29,13 @@
   text-align: center;
   color: #2c3e50;
 }
-
 nav {
   padding: 30px;
 }
-
 nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 nav a.router-link-exact-active {
   color: #42b983;
 }
